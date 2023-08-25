ffd6e8b607a1bb8044274bd6dc39ffd85267a1da
Kalpana <46937929+kkalpana55@users.noreply.github.com>
2023-08-25 10:27:41 -0400

    Update testcaseDE.spec.js

--- Specs/testcaseDE.spec.js
+++ Specs/testcaseDE.spec.js
@@ -19,7 +19,7 @@ describe('Applitools demo page',()=> {
    let casespage=null;
     beforeAll( async ()=>{
         // we launch browser and navigate to the loginpage
-        browser = await chromium.launch({ headless: false });
+        browser = await chromium.launch({ headless: true });
         context = await browser.newContext();
         page = await context.newPage();
       
